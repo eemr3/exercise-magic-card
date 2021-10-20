147c8a75473ff7bc8605121a4a466b80baafefd3
Emerson <eemr3@yahoo.com.br>
2021-10-19 22:49:42 -0300

    Exercícios do dia 9.3 Bonus - requisito 2 e Bounus

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -3,23 +3,35 @@ const favoriteCards = require('../data/favoriteCards.js');
 
 const retrievesFavoriteCards = () => {
   // implemente sua função aqui
+  favoriteCards.splice(4, favoriteCards.length - 4);
 }
 
 describe(' Testa a função getMagicCard', () => { 
+    afterEach(() => {
+      retrievesFavoriteCards();
+    });
 
   it('Testa se um novo card é adicionado a cada execução', async () => {
-    expect.assertions();
+    expect.assertions(2);
     await getMagicCard('130553');
-
+    
     // implemente seus testes aqui
-
+    const lastPosition = favoriteCards.length - 1;
+    expect(favoriteCards.length).toBe(5);
+    expect(favoriteCards[lastPosition].name).toBe('Beacon of Immortality');
   });
 
   it('Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
-    // expect.assertions();
-
-    // expect(favoriteCards).toHaveLength(4);
+    expect.assertions(2);
+    expect(favoriteCards).toHaveLength(4);
     // implemente seus testes aqui
-
+    const nameFavoriteCards = favoriteCards.map((favoriteCard) => favoriteCard.name);
+    expect(nameFavoriteCards).toEqual([
+      "Ancestor's Chosen",
+      "Angel of Mercy",
+      "Aven Cloudchaser",
+      "Ballista Squad",
+    ]);
   });
+  
 });
